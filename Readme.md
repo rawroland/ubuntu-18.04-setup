5ffc1900e2d6cefc30b22d5ac6fffe1ba1f4c787
Roland Awemo <rawroro@gmail.com>
2018-11-17 21:37:42 +0100

    :hankey: dirty commit

---
+++ Readme.md
@@ -0,0 +1,33 @@
+# What is this?
+A tool for automating the orchestration of Ubuntu 18.04 desktop using ansible.
+
+# What will be installed?
+
+## Communication
+*
+## Development
+* ant
+* apache2
+* atom
+* docker
+* filezilla
+* git
+* gource
+
+## Internet
+* firefox
+* google-chrome
+* vivaldi
+
+
+## Utility
+* autoconf
+* automake
+* build-essential
+* curl
+* byobu
+* python-dev
+* wget
+
+## Misc
+* gimp
---
+++ install.sh
@@ -0,0 +1,17 @@
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+if ! `which ansible >/dev/null 2>&1`; then
+  echo "#### Install ansible ####"
+  apt-get udpate
+  apt-get install -y software-properties-common python-minimal python python-pip sshpass
+  pip install pip --upgrade
+  pip install setuptools --upgrade
+  pip install pyopenssl ndg-httpsclient pyasn1
+  pip install ansible
+fi;
+
+mkdir /etc/ansible
+touch /etc/ansible/hosts
+echo "\n[local]\n127.0.0.1 ansible_connection=local" | tee /etc/ansible/hosts
+
+ansible-playbook "$parent_path/ansible/playbook.yml"
